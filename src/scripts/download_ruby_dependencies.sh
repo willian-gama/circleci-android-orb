0567f982f833f35388b8070215784404f5f1fe8e
williangama <>
2024-04-08 16:41:33 +0100

    remove all resources

--- src/scripts/download_ruby_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gem install bundler
-bundle check || bundle install --path vendor/bundle
--- src/scripts/save_unit_tests_results.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/test-results/unit-tests/
-find . -type f -regex ".*/build/test-results/.*xml" -exec cp {} ~/test-results/unit-tests/ \;
