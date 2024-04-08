84e5154c9af6a854cd2ae2ded692a0c48a416cbb
williangama <>
2024-04-08 14:31:00 +0100

    update private circleci orb to run unit tests

---
+++ src/scripts/generate-gradle-checksum.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -f "$FILE_NAME" ]; then
+  rm "$FILE_NAME"
+fi
+
+touch "$FILE_NAME"
+
+FILES=()
+while read -r -d ''; do
+	FILES+=("$REPLY")
+done < <(find . -type f \( -name "build.gradle*" -o -name "gradle-wrapper.properties" -o -name "settings.gradle*" \) -print0)
+
+for FILE in "${FILES[@]}"; do
+   CHECKSUM_FILE=$(openssl md5 "$FILE" | awk '{print $2}')
+   echo "$FILE - $CHECKSUM_FILE"
+   echo "$CHECKSUM_FILE" >> "$FILE_NAME"
+done
+
+sort "$FILE_NAME" -o "$FILE_NAME"
---
+++ src/scripts/generate_user_properties.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+USER_PROPERTIES="user.properties"
+export USER_PROPERTIES
+
+if [ ! -f "$USER_PROPERTIES" ]; then
+    echo "Creating user properties file..."
+    touch $USER_PROPERTIES
+
+    {
+      echo "maven_repo_url=$MAVEN_REPO_URL"
+      echo "maven_repo_username=$MAVEN_REPO_USERNAME"
+      echo "maven_repo_access_token=$MAVEN_REPO_ACCESS_TOKEN"
+      echo "sonar_token=$SONAR_TOKEN"
+      echo "sonar_project_key=$SONAR_PROJECT_KEY"
+      echo "sonar_organization_key=$SONAR_ORGANIZATION_KEY"
+      echo "sonar_analysis=$SONAR_ANALYSIS"
+    } >> $USER_PROPERTIES
+
+    cat $USER_PROPERTIES
+fi
