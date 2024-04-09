40533eb0c3aaa06c03a847e606b28566aca06175
williangama <willian_gama@live.com>
2024-04-09 15:08:08 +0100

    update scripts

--- src/scripts/run_code_linting_tools.sh
+++ src/scripts/run_code_linting_tools.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-./gradlew ktlintCheck detektFullDebug detektFullDebugUnitTest detektFullDebugAndroidTest lintFullDebug
+case $BUILD_VARIANT in
+  "debug") ./gradlew ktlintCheck detektDebug detektDebugUnitTest detektDebugAndroidTest lintDebug;;
+  "fullDebug") ./gradlew ktlintCheck detektFullDebug detektFullDebugUnitTest detektFullDebugAndroidTest lintFullDebug;;
+esac
