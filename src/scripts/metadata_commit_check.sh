fdcefb08f55597098ba43ac7d28d3d7be0119435
williangama <willian_gama@live.com>
2024-04-10 15:51:40 +0100

    add play_store_deploy job

---
+++ src/scripts/metadata_commit_check.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$(git status fastlane/metadata/ --porcelain)" ]; then
+  echo "Contains metadata updates"
+  git config user.email "engineering+mobiledeploy@kitmanlabs.com"
+  git config user.name "Mobile Deployer"
+  git add fastlane/metadata/*
+  git commit -m "[skip ci] Updated metadata"
+  git push origin "$CIRCLE_BRANCH"
+fi
