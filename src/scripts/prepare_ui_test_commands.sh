e697e7a379c23b9330a0da359b7b0da14571c9cb
williangama <willian_gama@live.com>
2024-04-11 19:46:45 +0100

    attempt to fix script

--- src/scripts/prepare_ui_test_commands.sh
+++ src/scripts/prepare_ui_test_commands.sh
@@ -3,17 +3,17 @@
 case $BUILD_VARIANT in
   "debug")
     {
-        echo "export UI_ASSEMBLE_COMMAND='assembleDebugAndroidTest'"
-        echo "export UI_TEST_COMMAND='connectedDebugAndroidTest'"
+        echo 'export UI_ASSEMBLE_COMMAND="assembleDebugAndroidTest"'
+        echo 'export UI_TEST_COMMAND="connectedDebugAndroidTest"'
     } >> "$BASH_ENV"
   ;;
   "fullDebug")
     {
-        echo "export UI_ASSEMBLE_COMMAND='assembleFullDebugAndroidTest'"
-        echo "export UI_TEST_COMMAND='connectedFullDebugAndroidTest'"
+        echo 'export UI_ASSEMBLE_COMMAND="assembleFullDebugAndroidTest"'
+        echo 'export UI_TEST_COMMAND="connectedFullDebugAndroidTest"'
     } >> "$BASH_ENV"
   ;;
 esac
 
-echo "$UI_ASSEMBLE_COMMAND"
-echo "$UI_TEST_COMMAND"
+echo "$BASH_ENV"
+cat "$BASH_ENV"
