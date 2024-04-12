48048540f40922b931d131ebfdb5e4a3ad7e827d
williangama <willian_gama@live.com>
2024-04-12 13:18:59 +0100

    test script

--- src/scripts/generate_gradle_checksum.sh
+++ src/scripts/generate_gradle_checksum.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-RESULT_FILE=$FILE_NAME
+run_with_retry() {
+   echo "$PARAM_PRE_TEST_COMMAND"
+   ${PARAM_PRE_TEST_COMMAND}
+}
 
-if [ -f "$RESULT_FILE" ]; then
-  rm "$RESULT_FILE"
-fi
-
-touch "$RESULT_FILE"
-
-FILES=()
-while read -r -d ''; do
-   FILES+=("$REPLY")
-done < <(find . -type f \( -name "build.gradle*" -o -name "gradle-wrapper.properties" -o -name "settings.gradle*" \) -print0)
-
-for FILE in "${FILES[@]}"; do
-   CHECKSUM_FILE=$(openssl md5 "$FILE" | awk '{print $2}')
-   echo "$FILE - $CHECKSUM_FILE"
-   echo "$CHECKSUM_FILE" >> "$RESULT_FILE"
-done
-
-sort "$RESULT_FILE" -o "$RESULT_FILE"
+run_with_retry
