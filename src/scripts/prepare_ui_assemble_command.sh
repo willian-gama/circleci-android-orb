25e73651b7f150f186f2f0c590bbb57820506872
williangama <willian_gama@live.com>
2024-04-12 12:11:17 +0100

    update script

---
+++ src/scripts/prepare_ui_assemble_command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+case $BUILD_VARIANT in
+  "debug") COMMAND="assembleDebugAndroidTest" ;;
+  "fullDebug") COMMAND="assembleFullDebugAndroidTest" ;;
+esac
+
+echo "export UI_ASSEMBLE_COMMAND=$COMMAND" >> "$BASH_ENV"
