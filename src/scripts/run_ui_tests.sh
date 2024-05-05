bb78346f848d2572b174761e5825977c73e009a7
williangama <willian_gama@live.com>
2024-05-05 21:00:27 +0100

    fix indentation

--- src/scripts/run_ui_tests.sh
+++ src/scripts/run_ui_tests.sh
@@ -40,11 +40,11 @@ group_ui_tests_per_module() {
       awk -F "^$MODULE." '{
         print($2)
       }' ORS=","
-   )
+    )
 
-   if [ -n "$UI_TEST_CLASS_NAMES" ]; then
+    if [ -n "$UI_TEST_CLASS_NAMES" ]; then
       run_ui_tests_with_retry "${MODULE//\./:}:$UI_TEST_COMMAND -Pandroid.testInstrumentationRunnerArguments.class=$UI_TEST_CLASS_NAMES"
-   fi
+    fi
   done
 }
 
