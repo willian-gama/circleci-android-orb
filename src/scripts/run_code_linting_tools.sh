b80632d0a173de131e85b79ec12cdf4f7b10d4aa
williangama <>
2024-05-24 19:04:00 +0100

    add word

--- src/scripts/run_code_linting_tools.sh
+++ src/scripts/run_code_linting_tools.sh
@@ -2,5 +2,5 @@
 
 COMMAND="ktlintCheck detekt lint$BUILD_VARIANT"
 echo "Running command: $COMMAND"
-# shellcheck disable=SC2086 # Do not expand gradle command because it has spaces.
+# shellcheck disable=SC2086 # Do not expand gradle command intentionally because it has spaces.
 ./gradlew $COMMAND
