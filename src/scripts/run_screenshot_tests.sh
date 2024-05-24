56775abf9ccca24188b79276f9a4dcccdaeda35c
williangama <>
2024-05-24 19:04:19 +0100

    update message

--- src/scripts/run_screenshot_tests.sh
+++ src/scripts/run_screenshot_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-COMMAND=verifyPaparazzi"$BUILD_VARIANT"
-echo "Running screenshot tests: $COMMAND"
+COMMAND=verifyPaparazzi$BUILD_VARIANT
+echo "Running command: $COMMAND"
 ./gradlew "$COMMAND"
