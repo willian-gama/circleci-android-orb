73c06d410102006385ad13467e7473179e8baf67
williangama <>
2024-05-24 18:21:41 +0100

    update ui tests job

--- src/scripts/prepare_ui_assemble_command.sh
+++ src/scripts/prepare_ui_assemble_command.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-COMMAND=assemble"$BUILD_VARIANT"AndroidTest
-echo "export UI_ASSEMBLE_COMMAND=$COMMAND" >> "$BASH_ENV"
+ASSEMBLE_TEST_COMMAND=assemble"$BUILD_VARIANT"AndroidTest
+UI_ASSEMBLE_COMMAND=$(
+  echo "$SPLIT_UI_TEST_CLASS_NAMES" |
+  awk -v ASSEMBLE_TEST_COMMAND="$ASSEMBLE_TEST_COMMAND" '{
+    for (i=1; i<=NF; i++) {
+      sub(/\..*/, "", $i)
+      print($i":"ASSEMBLE_TEST_COMMAND)
+    }
+  }' |
+  awk '!visited[$0]++' ORS=" "
+)
+
+echo "export UI_ASSEMBLE_COMMAND='$UI_ASSEMBLE_COMMAND'" >> "$BASH_ENV"
+cat "$BASH_ENV"
---
+++ src/scripts/split_ui_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SPLIT_UI_TEST_CLASS_NAMES=$(
+  circleci tests glob "**/androidTest/**/*Test.kt" "**/androidTest/**/*Test.java" |
+  sed "s@/@.@g" |
+  sed "s/src.androidTest.kotlin.//" | sed "s/src.androidTest.java.//" |
+  sed "s/.kt//" | sed "s/.java//" |
+  circleci tests split --split-by=name --timings-type=classname
+)
+
+echo "export SPLIT_UI_TEST_CLASS_NAMES='$SPLIT_UI_TEST_CLASS_NAMES'" >> "$BASH_ENV"
+cat "$BASH_ENV"
