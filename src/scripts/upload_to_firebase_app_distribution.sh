1484161ed7c485e06517841de6d53adb3fc4ad56
williangama <>
2024-05-24 19:05:41 +0100

    update message

--- src/scripts/upload_to_firebase_app_distribution.sh
+++ src/scripts/upload_to_firebase_app_distribution.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./gradlew assembleRelease appDistributionUploadRelease
+COMMAND="assemble$BUILD_VARIANT appDistributionUpload$BUILD_VARIANT"
+# shellcheck disable=SC2086 # Do not expand gradle command intentinally because it has spaces.
+./gradlew $COMMAND
