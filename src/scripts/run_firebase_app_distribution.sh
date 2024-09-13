5555aa8f604ac5cc652997bb4f164eb0321225fc
willian-gama <>
2024-09-13 12:37:31 +0100

    fix typos

--- src/scripts/run_firebase_app_distribution.sh
+++ src/scripts/run_firebase_app_distribution.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 COMMAND="assemble$BUILD_VARIANT appDistributionUpload$BUILD_VARIANT"
-# shellcheck disable=SC2086 # Do not expand gradle command intentinally because it has spaces.
+# shellcheck disable=SC2086 # Do not expand gradle command intentionally because it has spaces.
 ./gradlew $COMMAND
