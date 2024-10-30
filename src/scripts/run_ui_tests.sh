3e0f27e0e26713182536deddb8e8dd09e339fd5b
Willian Gama <10974743+willian-gama@users.noreply.github.com>
2024-10-30 22:43:44 +0000

    update scripts

--- src/scripts/run_ui_tests.sh
+++ src/scripts/run_ui_tests.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-UI_TEST_FAILURE=0
+ui_test_failure=0
 
 run_ui_tests_with_retry() {
-  TRIES=1
-  until [ $TRIES -gt "$MAX_TRIES" ]; do
-    echo "Starting test attempt $TRIES with command $1"
-    # shellcheck disable=SC2086 # Do not expand gradle command intentionally because it has spaces.
-    ./gradlew $1 && break
-    TRIES=$((TRIES+1))
+  tries=1
+  until [ $tries -gt "$MAX_TRIES" ]; do
+    echo "Starting test attempt $tries with command $1 $2"
+    ./gradlew "$1" "$2" && break
+    tries=$((tries+1))
     sleep "$RETRY_INTERVAL"
   done
 
-  if [ $TRIES -gt "$MAX_TRIES" ]; then
+  if [ $tries -gt "$MAX_TRIES" ]; then
     echo "Max tries reached ($MAX_TRIES)"
-    UI_TEST_FAILURE=1
+    ui_test_failure=1
   fi
 }
 
 group_ui_tests_per_module() {
-  UI_TEST_COMMAND=connected${BUILD_VARIANT}AndroidTest
-  MODULES=$(
+  ui_test_command=connected${BUILD_VARIANT}AndroidTest
+  modules=$(
     echo "$SPLIT_UI_TEST_CLASS_NAMES" |
     awk '{
       for (i=1; i<=NF; i++) {
@@ -31,26 +30,27 @@ group_ui_tests_per_module() {
     awk '!visited[$0]++' ORS=" "
   )
 
-  read -r -a MODULES <<< "$MODULES"
+  IFS=' ' read -r -a modules <<< "$modules"
 
-  for MODULE in "${MODULES[@]}"; do
-    UI_TEST_CLASS_NAMES=$(
+  for module in "${modules[@]}"; do
+    ui_test_class_names=$(
       echo "$SPLIT_UI_TEST_CLASS_NAMES" |
-      awk "/^$MODULE\./" |
-      awk -F "^$MODULE." '{
-        print($2)
-      }' ORS=","
+      tr ' ' '\n' |
+      awk "/^$module\./" |
+      awk -F "^$module." '{ print($2) }' ORS=","
     )
 
-    if [ -n "$UI_TEST_CLASS_NAMES" ]; then
-      run_ui_tests_with_retry "${MODULE//\./:}:$UI_TEST_COMMAND -Pandroid.testInstrumentationRunnerArguments.class=$UI_TEST_CLASS_NAMES"
+    if [ -n "$ui_test_class_names" ]; then
+      run_ui_tests_with_retry "${module//\./:}:$ui_test_command" "-Pandroid.testInstrumentationRunnerArguments.class=$ui_test_class_names"
+    else
+      echo "No UI test found for module: $module"
     fi
   done
 }
 
 group_ui_tests_per_module
 
-if [ "$UI_TEST_FAILURE" -eq 1 ]; then
+if [ "$ui_test_failure" -eq 1 ]; then
   echo "UI test failed"
   exit 1
 fi
