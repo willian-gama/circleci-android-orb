bcfea3dee23d1f5116661b5799637638e8d3efd2
willian-gama <>
2025-01-02 16:41:18 +0000

    update scripts

--- src/scripts/avd_define_name.sh
+++ src/scripts/avd_define_name.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-AVD_NAME=TestAVD-$CIRCLE_NODE_INDEX
+AVD_NAME="TestAVD-$CIRCLE_NODE_INDEX"
+AVD_DEVICE="pixel_3a_xl"
+AVD_SYSTEM_IMAGE="system-images;android-34;google_apis;x86_64"
 
-echo "AVD_NAME=$AVD_NAME"
-echo "export AVD_NAME='$AVD_NAME'" >> "$BASH_ENV"
+{
+  echo "export AVD_NAME='$AVD_NAME'"
+  echo "export AVD_DEVICE='$AVD_DEVICE'"
+  echo "export AVD_SYSTEM_IMAGE='$AVD_SYSTEM_IMAGE'"
+} >> "$BASH_ENV"
